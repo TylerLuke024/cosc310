7f100b5f4b53a751974561bbf20b581e02976ec3
Tyler <tylerluke024@gmail.com>
2026-02-03 15:50:22 -0600

    test commit

--- chapter9/lab2.java
+++ chapter9/lab2.java
@@ -1,21 +1,33 @@
 package chapter9;
 
-public class lab2 {
+import my.util.DLinkedList;
 
     public static void main(String[] args) {
         Stack<String> airports = new ArrayStack<>();
+        System.out.println(airports);
         airports.push("BHM");
+        System.out.println(airports);
         airports.push("ATL");
-        airports.push("LAX");
-
-        if(!airports.isEmpty()) {
+        System.out.println(airports);
+        airports.push("MSP");
+        System.out.println(airports);
+        if (!airports.isEmpty()) {
             String airport1 = airports.pop();
+            System.out.println(airports);
             String airport2 = airports.pop();
+            System.out.println(airports);
             System.out.println(airport1);
             System.out.println(airport2);
         }
-        System.out.println(airports);
+        airports.pop();
 
+        DLinkedList<String> list = new DLinkedList<>();
+        System.out.println(list);
+        list.add("BHM");
+        System.out.println(list);
+        list.add("ATL");
+        System.out.println(list);
+        list.add("MSP");
+        System.out.println(list);
     }
-
 }
