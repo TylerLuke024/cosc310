82737107f94c4921ff328289b16391ad182088c9
Tyler <tylerluke024@gmail.com>
2026-02-03 15:52:36 -0600

    test

--- chapter9/lab2.java
+++ chapter9/lab2.java
@@ -2,6 +2,7 @@ package chapter9;
 
 import my.util.DLinkedList;
 
+public class Lab2 {
     public static void main(String[] args) {
         Stack<String> airports = new ArrayStack<>();
         System.out.println(airports);
