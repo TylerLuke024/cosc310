eea2bf2c4a8d7cf616cd54fd5c132037986278b5
Tyler <tylerluke024@gmail.com>
2026-02-10 12:48:01 -0600

    capital

--- chapter9/Lab2.java
+++ chapter9/Lab2.java
@@ -3,7 +3,7 @@ package chapter9;
 import my.util.DLinkedList;
 
 public class Lab2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Stack<String> airports = new ArrayStack<>();
         System.out.println(airports);
         airports.push("BHM");
